d5907553a53f4f5d3526c7dd40a55a9d16698d1d
ujjwalhat <ujjwal31072002@gmail.com>
2024-06-21 23:39:26 +0530

    first

---
+++ src/components/Skills.js
@@ -0,0 +1,83 @@
+import { Container, Col, Row } from "react-bootstrap";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import colorSharp from "../assets/img/color-sharp.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Skills = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  return (
+    <section className="skill" id="skills">
+          <Container>
+            <Row>
+          <Col>
+        <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__backInRight" : "animate__animated animate__backOutLeft"}>
+            <div className="skill-bx">
+              <h2>Skills</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur<br></br> adipisicing
+                elit. Velit possimus dolores natus eveniet alias necessitatibus
+                magni dicta quas quibusdam officiis voluptatibus, illo quaerat?
+                Repudiandae, iusto labore? Nemo vitae consequuntur doloribus.
+              </p>
+              <Carousel
+                responsive={responsive}
+                infinite={true}
+                className="skill-slider"
+              >
+                <div className="item">
+                  <img src={meter1} alt="Images" />
+                  <h5>Web Developer</h5>
+                </div>
+                <div className="item">
+                  <img src={meter2} alt="Images" />
+                  <h5>Flutter App Developer</h5>
+                </div>
+                <div className="item">
+                  <img src={meter3} alt="Images" />
+                  <h5>UI/UX</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="Images" />
+                  <h5>Python Developer</h5>
+                </div>
+              </Carousel>
+            </div>
+      </div>}
+      </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img
+        className="background-image-left"
+        src={colorSharp}
+        alt="Skills Wait"
+      />
+    </section>
+  );
+};
